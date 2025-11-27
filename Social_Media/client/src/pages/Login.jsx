1c43286337e5f92829067137e5b90738f82e9276
AZZAMAF <aafazzam@gmail.com>
2025-11-27 21:39:40 +0700

    Login Half

--- Social_Media/client/src/pages/Login.jsx
+++ Social_Media/client/src/pages/Login.jsx
@@ -14,13 +14,23 @@ const Login = () => {
           <div className='flex items-center gap-2 mb-4 max-md:mt-10'>
             <img src={assets.group_users} alt="" className='h-8 md:h-10'/>
             <div>
-              <div>
-                {Array(5).fill(0).map((_,i) =>(<Star key={i} className=' size-4 md:size-4.5 text-transparent fill-amber-500'/>))}
+              <div className='flex'>
+                {Array(5).fill(0).map((_,i) =>(<Star key={i} 
+                className=' size-4 md:size-4.5 text-transparent fill-amber-500'/>))}
               </div>
+              <p>Used by 12+ developers</p>
             </div>
           </div>
+          <h1 className='text-3xl md:text-6xl md:pb-2
+          font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 
+          bg-clip-text text-transparent'>More than just friends truly connect</h1>
+          <p className='text-3xl text-indigo-900 max-w-72 md:max-w-md'>
+            Connect with global community on pingUP.</p>
         </div>
+        <span className='md:h-10'></span>
       </div>
+      {/* Right Side: Login Form */}
+      <div className='flex-1 flex items-center justify-center p-6 sm:p-10'></div>
     </div>
   )
 }

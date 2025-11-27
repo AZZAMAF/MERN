f90452c177edcf8cdf724b0ee519026af9cde2ac
AZZAMAF <aafazzam@gmail.com>
2025-11-27 14:51:02 +0700

    first

--- MERN_Auth/server/config/mongodb.js
+++ MERN_Auth/server/config/mongodb.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async ()=>{
+    mongoose.connection.on('connected', ()=> console.log('Database Connected'));
+    await mongoose.connect(`${process.env.MONGODB_URI}/MERN_Auth`);
+};
 
-    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/MERN_Auth`)
-}
+export default connectDB    

e5bba4ea285abf4bf437a4e4fb1d27e055d18b57
AZZAMAF <aafazzam@gmail.com>
2025-12-01 15:30:42 +0700

    Story Section

--- Social_Media/client/src/components/StoriesBar.jsx
+++ Social_Media/client/src/components/StoriesBar.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { dummyStoriesData } from '../assets/assets'
 import { Plus } from 'lucide-react'
+import moment from 'moment'
+import StoryModel from './StoryModel'
+import StoryViewer from './StoryViewer'
 
 
 
 const StoriesBar = () => {
     
     const [stories, setStories] = useState([])
+    const [showModal, setShowModal] = useState(false)
+    const [viewStory, setViewStory] = useState(null)
 
     const fetchStories = async (params) =>{
         setStories(dummyStoriesData)
@@ -17,11 +22,11 @@ const StoriesBar = () => {
     },[])
 
   return (
-    <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scrollbar overflow-x-auto px-4'> 
+    <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scrollbar overflow-x-auto px-4 '> 
     
-    <div className='flex gap-4 pb-5'>
+    <div className='flex gap-4 pb-5 '>
         {/* Add Story Card */}
-        <div className='rounded-lg shadow-sm min-w-[7.5rem] max-w-[7.5rem] max-h-40 aspect-[3/4]
+        <div onClick={()=> setShowModal(true)} className='rounded-lg shadow-sm min-w-[7.5rem] max-w-[7.5rem] max-h-40 aspect-[3/4]
         cursor-pointer hover:shadow-lg transition-all duration-200 border-2 border-dashed
         border-indigo-300 bg-gradient-to-b from-indigo-50 to-white'>
             <div className='h-full flex flex-col items-center justify-center p-4'>
@@ -34,7 +39,46 @@ const StoriesBar = () => {
                 </p>
             </div>
         </div>
+        {/* Story Cards */}
+        {
+            stories.map((story,index)=>(
+                <div onClick={()=> setViewStory(story)}key={index} className={`relative rounded-lg shadow min-w-30 max-h-40 cursor-pointer 
+                hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500
+                to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95 `}>
+                    <img src={story.user.profile_picture} alt="" 
+                    className='absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow'/>
+                    <p className=' absolute top-18 left-3 text-white/60 text-sm truncate max-w-24'>
+                        {story.content}</p>
+                    <p className='text-white absolute bottom-1 right-2 z-10 text-xs'>
+                        {moment(story.createdAt).fromNow()}
+                    </p>
+                    {
+                        story.media_type !== 'text' &&(
+                            <div className='absolute inset-0 z-1 rounded-lg bg-black overflow-hidden'>
+                                {
+                                    story.media_type === "image" ?
+                                    <img src={story.media_url} alt="" className='h-full w-full
+                                    object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-0' />
+                                    :
+                                    <video src={story.media_url} className='h-full w-full object-cover hover:scale-110
+                                    transition duration-500 opacity-70 hover:opacity-80'/>
+                                }
+
+                            </div>
+                        )
+                    }
+                </div>
+            ))
+        }
     </div>
+    {/* StoryModel */}
+    {
+        showModal && <StoryModel setShowModal={setShowModal} fetchStories={fetchStories}/>
+    }
+    {/* View Story Modal */}
+    {
+        viewStory && <StoryViewer viewStory ={viewStory} setViewStory = {setViewStory} />
+    }
 </div>
   )
 }

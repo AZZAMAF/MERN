e77b970645b03416db67b9ddbc61e36cb323f398
AZZAMAF <aafazzam@gmail.com>
2025-12-12 13:18:42 +0700

    update server

--- Social_Media/server/server.js
+++ Social_Media/server/server.js
@@ -12,7 +12,9 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/', (reg, res) => res.send('Server is running'))
-app.use("/api/inngest", serve({ client: inngest, functions }) );
+app.use("/api/inggest", serve({ client: inngest, functions }) );
 
 const PORT = process.env.PORT || 4000 ;
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+
+
